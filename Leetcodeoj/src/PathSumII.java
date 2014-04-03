ddd045ef4978785f7c9fc59cfb92b20c63363fbc
czhoume <czhoume@gmail.com>
2014-04-03 19:07:42 -0400

    added TriangleMinSum

--- Leetcodeoj/src/PathSumII.java
+++ Leetcodeoj/src/PathSumII.java
@@ -1,14 +1,52 @@
-/**
- * Definition for binary tree
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+
 public class PathSumII {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
     public ArrayList<ArrayList<Integer>> pathSum(TreeNode root, int sum) {
-        
+    	ArrayList<ArrayList<Integer>> res =  new ArrayList<ArrayList<Integer>>();
+    	HashMap<TreeNode, ArrayList<Integer>> m = new HashMap<TreeNode, ArrayList<Integer>>();
+    	LinkedList<TreeNode> l = new LinkedList<TreeNode>();
+    	ArrayList<Integer> t = new ArrayList<Integer>();
+    	if(root==null){
+    	    return res;
+    	}
+    	t.add(root.val);
+    	t.add(root.val);
+    	m.put(root, t);
+    	l.add(root);
+    	while(!l.isEmpty()){
+    		TreeNode tmp = l.remove();
+    		if(tmp.left!=null){
+    			ArrayList<Integer> tmpArray = new ArrayList<Integer>(m.get(tmp));
+    			tmpArray.add(tmp.left.val);
+    			int tmpSum=tmpArray.get(0);
+    			tmpSum+=tmp.left.val;
+    			tmpArray.set(0, tmpSum);
+    			m.put(tmp.left, tmpArray);	
+    			l.add(tmp.left);
+    		}
+    		if(tmp.right!=null){
+    			ArrayList<Integer> tmpArray = new ArrayList<Integer>(m.get(tmp));
+    			tmpArray.add(tmp.right.val);
+    			int tmpSum=tmpArray.get(0);
+    			tmpSum+=tmp.right.val;
+    			tmpArray.set(0, tmpSum);
+    			m.put(tmp.right, tmpArray);	
+    			l.add(tmp.right);
+    		}
+    		if(tmp.left==null && tmp.right==null){
+    			ArrayList<Integer> tmpArray = m.get(tmp);
+    			if(tmpArray.get(0)==sum){
+    				tmpArray.remove(0);
+    				res.add(tmpArray);
+    			}
+    		}
+    	}
+    	return res;
     }
 }
---
+++ Leetcodeoj/src/TriangleMinSum.java
@@ -0,0 +1,37 @@
+public class TriangleMinSum {
+    public int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
+        int min;
+        ArrayList<Integer> s1 = new ArrayList<Integer>();
+        ArrayList<Integer> s2 = new ArrayList<Integer>();
+        s2.add(triangle.get(0).get(0));
+        for(int i=1; i< triangle.size(); i++){
+        	ArrayList<Integer> tmp=triangle.get(i);
+        	if(i%2==0){
+        		s1.clear();
+        		for(int j=0; j<tmp.size(); j++){
+        			if(j==0){
+        				s1.add(tmp.get(j)+s2.get(0));
+        			}else if(j==tmp.size()-1){
+        				s1.add(tmp.get(j)+s2.get(j-1));
+        			}else{
+        				int tmpmin=Math.min(s2.get(j), s2.get(j-1));
+        				s1.add(tmpmin+tmp.get(j));
+        			}
+        		}
+        	}else{
+        		s2.clear();
+        		for(int j=0; j<tmp.size(); j++){
+        			if(j==0){
+        				s2.add(tmp.get(j)+s1.get(0));
+        			}else if(j==tmp.size()-1){
+        				s2.add(tmp.get(j)+s1.get(j-1));
+        			}else{
+        				int tmpmin=Math.min(s1.get(j), s1.get(j-1));
+        				s2.add(tmpmin+tmp.get(j));
+        			}
+        		}
+        	}
+        }
+        ArrayList<Integer> bot = (triangle.size()%2==0)?s1:s2;
+    }
+}
