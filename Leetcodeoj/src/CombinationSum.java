ff6b8dd70021c391e18505a2f367295b77e8c684
czhoume <czhoume@gmail.com>
2014-04-03 11:22:58 -0400

    CombinationSum Accepted started PathSumII

--- Leetcodeoj/src/CombinationSum.java
+++ Leetcodeoj/src/CombinationSum.java
@@ -5,22 +5,26 @@ public class CombinationSum {
     }
 
     public ArrayList<ArrayList<Integer>> getList(int[] candidates, int target, int index) {
-    	int i;
+        int i;
         ArrayList<ArrayList<Integer>> tmpres, res;
         res=new ArrayList<ArrayList<Integer>>();
         for(i=index; i<candidates.length; i++){
-        	if(target<candidates[i])
-        		continue;
-        	else if(target==candidates[i])
-        		res.add(candidates[i]);
-        	else{
-        		tmpres=getList(candidates, target-candidates[i], index);
-        		if(!tmpres.isEmpty){
-        			for(ArrayList<Integer> tmp:tmpres){
-        				res.add(tmp.add(candidates[i]));
-        			}
-        		}
-        	}
+            if(target<candidates[i])
+                continue;
+            else if(target==candidates[i]){
+                ArrayList<Integer> tmp=new ArrayList<Integer>();
+                tmp.add(candidates[i]);
+                res.add(tmp);
+            }
+            else{
+                tmpres=getList(candidates, target-candidates[i], i);
+                if(!tmpres.isEmpty()){
+                    for(ArrayList<Integer> tmp:tmpres){
+                        tmp.add(0, candidates[i]);
+                        res.add(tmp);
+                    }
+                }
+            }
         }
         return res;
     }
