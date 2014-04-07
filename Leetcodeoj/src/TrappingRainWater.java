bed705e59f840d8c8fedf1127e08c84663489f36
czhoume <czhoume@gmail.com>
2014-04-07 12:38:38 -0400

    half in TrappingRainWater

--- Leetcodeoj/src/TrappingRainWater.java
+++ Leetcodeoj/src/TrappingRainWater.java
@@ -1,4 +1,31 @@
 
 public class TrappingRainWater {
-
+    public int trap(int[] A) {
+        int n = A.length;
+        if(n==0)
+        	return 0;
+        int [] h = new int[n];
+        h[n-1]=n;
+        for(int i=n-2; i>=0; i--){
+        	int w;
+        	if(A[i]>A[i+1]){
+        		w=h[i+1];
+        		while(w!=n){
+        			if(A[i]>A[w]&&w!=n-1){
+        				w=h[w+1];
+        			}else{
+        				h[i]=w;
+        				break;
+        			}
+        		}
+        	}else{
+        		h[i]=i+1;
+        	}
+        }
+        int sum=0;
+        for(int i=0; i<n; i++){
+        	sum = sum + h[i] - i - 1;
+        }
+        return sum;
+    }
 }
