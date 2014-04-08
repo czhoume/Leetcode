355ed98497a24f0dd6a4d9ec5fb8054d479b51c6
czhoume <czhoume@gmail.com>
2014-04-08 17:49:40 -0400

    finished convert sorted list to bst

--- Leetcodeoj/src/ConvertSortedListToBST.java
+++ Leetcodeoj/src/ConvertSortedListToBST.java
@@ -17,6 +17,35 @@
  */
 public class Solution {
     public TreeNode sortedListToBST(ListNode head) {
-        
+    	if(head==null)
+    		return null;
+        ListNode mid=getMid(head, null);
+        TreeNode root = new TreeNode(mid.val);
+        root.left=build(head, mid);
+        root.right=build(mid.next, null);
+        return root;
+    }
+
+    public TreeNode build(ListNode head, ListNode end){
+    	ListNode mid=getMid(head, end);
+    	if(mid==null)
+    		return null;
+        TreeNode root = new TreeNode(mid.val);
+        root.left=build(head, mid);
+        root.right=build(mid.next, end);
+        return root;
+    }
+
+    public ListNode getMid(ListNode head, ListNode end){
+    	if(head==end)
+    		return null;
+    	if(head.next==end)
+    		return head;
+    	ListNode slow=head, fast=head;
+    	while((!fast.equals(end) && !fast.next.equals(end)) && fast.next!=null){
+    		fast=fast.next.next;
+    		slow=slow.next;
+    	}
+    	return slow;
     }
 }
